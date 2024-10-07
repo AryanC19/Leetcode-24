a9b9b1b69356d296479e7a398e69936e82027a52
Aryan C <89149374+AryanC19@users.noreply.github.com>
2024-10-07 21:04:10 +0530

    Time: 19 ms (8.22%), Space: 10.3 MB (23.32%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string st=to_string(x);
+        int l=0,r=st.size()-1;
+        while(l<r){
+            if(st[l++]!=st[r--]) return false;
+        }
+        return true;
+    }
+};
