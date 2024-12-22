b57b0fceb0589d5d3e3aa803543b1d67ec416fe3
Aryan C <89149374+AryanC19@users.noreply.github.com>
2024-12-22 20:03:17 +0530

    Time: 607 ms (7.14%), Space: 350.3 MB (7.14%) - LeetHub

--- 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.cpp
+++ 3394-check-if-grid-can-be-cut-into-sections/3394-check-if-grid-can-be-cut-into-sections.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
     bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
-        vector<vector<int>> xarr, yarr, mergedX, mergedY;
+        
+        vector<vector<int>> xarr,yarr,ans1,ans2;
 
-        // Separate intervals for x and y
-        for (auto& rect : rectangles) {
-            xarr.push_back({rect[0], rect[2]});
-            yarr.push_back({rect[1], rect[3]});
+
+        for(auto i:rectangles){
+            xarr.push_back({i[0],i[2]});
+            yarr.push_back({i[1],i[3]});
         }
 
-        // Sort the intervals
-        sort(begin(xarr), end(xarr));
-        sort(begin(yarr), end(yarr));
+        sort(begin(xarr),end(xarr));
+        sort(begin(yarr),end(yarr));
 
-        // Merge x intervals
-        for (auto& i : xarr) {
-            if (mergedX.empty() || mergedX.back()[1] <= i[0]) {
-                mergedX.push_back(i);
-            } else {
-                mergedX.back()[1] = max(mergedX.back()[1], i[1]);
+        //check for x parts
+
+/*
+Input: n = 5, rectangles = [[1,0,5,2],[0,2,2,4],[3,2,5,3],[0,4,4,5]]
+
+xarr= {0,2}{2,4}{2,3}{4,5} 
+
+ans1 ={0,2}
+*/
+        for(auto i:xarr){
+
+            if(ans1.empty() || ans1.back()[1] <= i[0]){
+                ans1.push_back(i);
+            }else if(ans1.back()[1] > i[0]){
+                ans1.back()[1]=max(ans1.back()[1],i[1]);
             }
         }
 
-        // Merge y intervals
-        for (auto& i : yarr) {
-            if (mergedY.empty() || mergedY.back()[1] <= i[0]) {
-                mergedY.push_back(i);
-            } else {
-                mergedY.back()[1] = max(mergedY.back()[1], i[1]);
+        for(auto i:yarr){
+
+            if(ans2.empty() || ans2.back()[1] <= i[0]){
+                ans2.push_back(i);
+            }else if(ans2.back()[1] > i[0]){
+                ans2.back()[1]=max(ans2.back()[1],i[1]);
             }
         }
 
-        // Check if we can make valid cuts in either direction
-        // Valid if we can divide into exactly 3 sections
-        bool validX = mergedX.size() >= 3;
-        bool validY = mergedY.size() >= 3;
 
-        return validX || validY;
+
+        int n1=ans1.size();
+        int m=ans2.size();
+
+        return n1>=3 || m>=3;
     }
 };
