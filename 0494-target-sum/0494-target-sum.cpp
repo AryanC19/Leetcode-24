d2d20ea139016f6c60cf4caed50d11db3f36300a
Aryan C <89149374+AryanC19@users.noreply.github.com>
2024-12-26 10:52:42 +0530

    Time: 3 ms (88.31%), Space: 11.8 MB (64.97%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,38 +1,59 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
     int n;
-    vector<vector<int>> dp;
-    int func(int i, vector<int>& arr, int k) {
-        if (k < 0) return 0; 
-        if (i == n) {
-            return (k == 0) ? 1 : 0; 
+    int dp[21][1001];
+    int func(int i,vector<int>& nums, int target){
+
+        if(i>=n && target==0) return 1;
+        else if(i>=n) return 0;
+
+        if(dp[i][target]!=-1) return dp[i][target];
+        int nottake=func(i+1,nums,target);
+
+
+        int take=0;
+
+        if(target>=nums[i]){
+
+            take=func(i+1,nums,target-nums[i]);
         }
+
+        return dp[i][target]=take+nottake;
+
         
-        if (dp[i][k] != -1) return dp[i][k];
-        
-        int nottake = func(i + 1, arr, k);
-        
-        int take = 0;
-        if (arr[i] <= k) {
-            take = func(i + 1, arr, k - arr[i]);
-        }
-        
-        return dp[i][k] = (take + nottake)% MOD;
     }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        /*
+        
+        target=d;
 
-    int findTargetSumWays(vector<int>& arr, int target) {
-        n = arr.size();
-        int sum= accumulate(begin(arr),end(arr),0);
+        s1+s2=total
+        s1=total-s2 --->1 
 
-       //if (abs(target) > sum) return 0;
-        if((sum-target)%2!=0) return 0;
-        int k=(sum-target)/2;
-        if(k < 0) return 0;  // Add this line before dp resize
-        
-        
-        
-        dp.resize(n+1, vector<int>(k + 1, -1)); 
-        return func(0, arr, k);        
+        s1-s2=d;  using 1
+
+        totoal -s2-s2 =d
+        total -2s2=d
+
+        s2=( total-d) /2
+
+
+
+       
+
+        */
+        n=nums.size();
+        int total=0;
+
+
+        memset(dp,-1,sizeof(dp));
+        for(auto i:nums) total+=i;
+        int s1= (-target+total)/2;
+if (s1 < 0) return 0;
+        // Check if target is achievable
+        if (abs(target) > total) return 0;
+        if ((target + total) % 2 != 0) return 0;
+
+        return func(0,nums,s1); 
     }
 };
