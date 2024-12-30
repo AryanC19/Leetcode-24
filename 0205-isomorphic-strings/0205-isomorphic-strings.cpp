f0bec83f5386eda79e8a6054b0cff17874b0799c
Aryan C <89149374+AryanC19@users.noreply.github.com>
2024-12-30 22:50:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
+    bool isIsomorphic(string s, string t) {
+        
+        unordered_map<char,char> mpp;
+        vector<bool> mapped(126,false);
 
-bool isIsomorphic(string a, string b)
-{
-    unordered_map<char, char> mp1;
-    unordered_map<char, bool> mp2;
+        int n=s.size();
+        int m=t.size();
 
-    int n = a.size();
-    int m = b.size();
-    if (n != m)
-        return false;
+        if(n!=m) return false;
 
-    for (int i = 0; i < n; i++)
-    {
 
-        char ch1 = a[i];
-        char ch2 = b[i];
-        if (mp1.find(ch1) != mp1.end())
-        {
-            if(mp1.at(ch1)!=ch2){
-                return false;
-
-            }
-        }else{
-            if(mp2.find(ch2)!=mp2.end()){
-                return false;
-            }else{
-                mp1[ch1] = ch2;
-                mp2[ch2] =true;
-
-            }
+        for(int i=0;i<n;i++){
+            if(mpp.find(s[i])==mpp.end() && !mapped[t[i]-'a'] ){
+                mpp[s[i]]=t[i];
+                mapped[t[i]-'a']=true;
+            }else if (mapped[t[i]-'a']) {
+                return mpp[s[i]]==t[i];
+            }else return false;
         }
+        return true;
     }
-    return true;
-}
 };
