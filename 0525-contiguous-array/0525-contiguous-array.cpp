233b5080550f72df4153c3e6fdacccf719749b2b
Aryan C <89149374+AryanC19@users.noreply.github.com>
2024-12-31 19:12:36 +0530

    Time: 20 ms (83.83%), Space: 88.3 MB (55.31%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -3,22 +3,20 @@ public:
     int findMaxLength(vector<int>& nums) {
         unordered_map<int,int> mpp;
 
+        int psum=0;
 
         mpp[0]=-1;
-        int ans=0;
-        int ps=0;
-        int n=nums.size();
+        int maxlen=0;
 
-        for(int i=0;i<n;i++){
-            ps+= nums[i]==1?1:-1;
 
-            if(mpp.count(ps)){
-                ans=max(ans,i- mpp[ps]);
+        for(int i=0;i<nums.size();i++){
+            psum+= nums[i]==1? 1:-1 ;
+            if(mpp.count(psum)){
+                maxlen=max(maxlen,i-mpp[psum]);
             }else{
-                mpp[ps]=i;
+                mpp[psum]=i;
             }
         }
-
-        return ans;
+        return maxlen;
     }
 };
