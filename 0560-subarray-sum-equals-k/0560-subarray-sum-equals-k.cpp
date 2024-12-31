abf9b01defd39e87fa7b704ef71033835147cfa6
Aryan C <89149374+AryanC19@users.noreply.github.com>
2024-12-31 18:12:32 +0530

    Time: 52 ms (53.65%), Space: 45.3 MB (66.58%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        
-        // sum, count of 
+        //           presum,freq of it occuring
         unordered_map<int,int> mpp;
-
-
-        int n=nums.size();
+        int ans=0,presum=0;
+        int n =nums.size();
+        // so that we account for the case of presum=0 has an occurence of 1
         mpp[0]=1;
-        int presum=0;
-        int ans=0;
-        for(auto i:nums){
-            presum+=i;
-            ans+=mpp[presum-k];
+        for(int i=0;i<n;i++){
+            
+            presum+=nums[i];
+
+            // if ps-k present, that means we have a subar of size K between 
+            // psA and ps B
+
+            /*
+            psA    
+            --------->
+                k
+                <---->
+            --->
+            psB
+
+            that means psA-psB=K
+            so the subarr with size K is present so increase count
+
+            map condition=  if (psA-k)!=mpp.end();  that is if theres an entry
+
+            psA-psB=K
+            psA-K=psB
+            
+            */
+            if(mpp.find(presum-k)!=mpp.end()){
+                ans+=mpp[presum-k];
+            }   
             mpp[presum]++;
         }
-
         return ans;
     }
 };
