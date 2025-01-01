1e1a80b7a987392fab8c67a9c530a2e43d731aa3
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-01-01 14:00:56 +0530

    Time: 43 ms (5.34%), Space: 10.4 MB (5.2%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -5,26 +5,26 @@ public:
         unordered_map<int,int> zeroLeft,oneRight;
         int n=s.size();
 
-        if(s=="01") return 2;
+     //   if(s=="01") return 2;
 
         int lz=0,ro=0;
 
         int score=0;
 
-        for(int i=1;i<n;i++){
-            lz+= s[i-1]=='0' ?1 :0;
-            zeroLeft[i-1]=lz;
+        for(int i=0;i<n-1;i++){
+            lz+= s[i]=='0' ?1 :0;
+            zeroLeft[i]=lz;
         }
 
-        for(int i=n-2;i>=0;i--){
-            ro+= s[i+1]=='1' ?1 :0;
-            oneRight[i+1]=ro;
+        for(int i=n-1;i>0;i--){
+            ro+= s[i]=='1' ?1 :0;
+            oneRight[i]=ro;
         }
 
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             cout<<zeroLeft[i]<< " "<< oneRight[i]<<endl;
-            score=max(score,zeroLeft[i]+oneRight[i]);
+            score=max(score,zeroLeft[i]+oneRight[i+1]);
         }
 
         return score;
