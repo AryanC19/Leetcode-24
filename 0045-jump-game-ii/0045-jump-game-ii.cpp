318567dd9dae5906a55b36b694afd5028b4dbf0a
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-01-02 14:19:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
+    int n;
+    int dp[1001];
+    int func(int i,vector<int>& nums){
 
-    /*
-     i
-     0 1 2 3 4
-    [2,3,1,1,4]
-    
-    */
+        if(i>=n-1) return 0;
+        if(dp[i]!=-1) return dp[i];
 
-    int dp[10001];
-    int func(int currInd,vector<int>& nums,int n){
-        if(currInd>=n-1) return 0;
-        if(dp[currInd]!=-1) return dp[currInd];
-        int minJumps=n;
-        for(int jumps=1;jumps<=nums[currInd];jumps++){
-            minJumps=min(minJumps,1+func(currInd+jumps,nums,n));
-        } 
-        return dp[currInd]=minJumps;
+        int minJumps=1e9;
+        int jumps=0;
+        for(int a=1;a<=nums[i];a++){
+            jumps=1+func(i+a,nums);
+            minJumps=min(minJumps,jumps);
+        }
+        
+        return dp[i]=minJumps;
+
     }
     int jump(vector<int>& nums) {
-
-        int n=nums.size();
+        n=nums.size();
         memset(dp,-1,sizeof(dp));
-        return func(0,nums,n);
-
+        return func(0,nums);
     }
 };
