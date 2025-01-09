5358d52025b7e754eceee7f6a47235de9a133319
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-01-09 10:18:05 +0530

    Time: 0 ms (100%), Space: 13 MB (85.23%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,11 +1,33 @@
 class Solution {
 public:
+    int returnIndex(string &word,string&pref){
+
+        int i=0;
+        int j=0;
+
+        int n=word.size();
+        int m=pref.size();
+
+        for(i=0;i<n;i++){
+            int j=i;
+            int k=0;
+            while(j<n &&k<m){
+                if(word[j]!=pref[k]){ break;}
+                else{j++;k++;}
+            }
+            if(k==m) return i;
+        }
+        return -1;
+
+    }
     int prefixCount(vector<string>& words, string pref) {
         
         int cnt=0;
 
         for(auto &word:words){
-            if(word.find(pref)==0) cnt++;
+           // if(word.find(pref)==0) cnt++;
+
+           if(returnIndex(word,pref)==0) cnt++;
         }
         return cnt;
     }
