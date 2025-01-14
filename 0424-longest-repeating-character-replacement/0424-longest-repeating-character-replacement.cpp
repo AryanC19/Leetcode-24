67d3ff521f4d91fdad7d345e290eb8b014d739af
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-01-14 14:37:46 +0530

    Time: 27 ms (10.94%), Space: 10.9 MB (63.42%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,31 +1,40 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-
         int n=s.size();
         
-        int ans=0;
-        for(char c='A';c<='Z';c++){
-            int l=0,r=0;
-            int T=0;
-            while(r<n){
-                T+= s[r]==c ?1:0;
+        //choose any character of the string and change
+        // it to any other uppercase English character
+        // no of uppercase English->26
 
-                // The key change is here - we need to check if replacements needed
-                // exceed k, not if matching characters exceed k instead (T>k),
-                while((r - l + 1) - T >k){
-                    T-=s[l]==c ?1:0;
+        // problem translates to length of longest substr with at max K replaced cahracters
+
+        int maxlen=0;
+
+
+        for(int i=0;i<26;i++){
+            char c='A'+i;
+            int l=0,r=0;
+
+            int T=0;
+
+            while(r<n){
+
+                if(s[r]==c) T++;
+
+// while the length of the current window minus the count of character 'c' exceeds K,
+// it means there are more than K characters in the window that are not 'c'.
+// to maintain at most K replacements, we need to shrink the window from the left. 
+                while((r-l+1)-T>k){
+                    T-= s[l]==c ?1:0;
                     l++;
                 }
-                // (r-l+1)-T denotes the length of the current string minus the length of the characters.
-                // which equals the len of replacements.
-
-                //tc O(26 *n) ~ O(n)
-                ans=max(ans,r-l+1);
+                maxlen=max(maxlen,r-l+1);
                 r++;
-            }   
+            }
         }
 
-        return ans;
+        return maxlen;
+
     }
 };
