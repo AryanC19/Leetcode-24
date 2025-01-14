b44f3f0aed3f411ea125a3e90087938f56c6e40f
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-01-14 23:00:46 +0530

    Time: 15 ms (25.26%), Space: 40.6 MB (11.1%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,41 +1,24 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    void solve(TreeNode* root, int targetSum,vector<vector<int>> &ans,vector<int> &ds){
-        
-        if(root==NULL) return ;
+    void func(TreeNode* root, int targetSum,int sum,vector<int> ds,vector<vector<int> > &ans){
+
+        if(root==NULL) return;
+        sum+=root->val;
         ds.push_back(root->val);
-
-        if(targetSum==root->val && root->left==NULL && root->right==NULL){
+        
+        if(root->left==NULL && root->right==NULL  && targetSum==sum){
             ans.push_back(ds);
-            
+            return;
         }
-
-
-        if(root->left)
-            solve(root->left,targetSum-root->val,ans,ds);
-        
-        if(root->right)      
-            solve(root->right,targetSum-root->val,ans,ds);
-        
-        ds.pop_back();
-    
+        func(root->left,targetSum,sum,ds,ans);
+        func(root->right,targetSum,sum,ds,ans); 
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> ans;
+        vector<vector<int> >ans;
         vector<int> ds;
 
-        solve(root,targetSum,ans,ds);
+        func(root,targetSum,0,ds,ans);
         return ans;
     }
 };
