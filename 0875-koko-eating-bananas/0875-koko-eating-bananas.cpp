9459df855fe086e13f01a2f6fd5a5698cd15ba28
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-01-27 21:16:27 +0530

    Time: 7 ms (75.77%), Space: 23 MB (14.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    typedef long long ll;
-    bool func(ll speed, vector<int>& piles, ll limit){
-        ll hours=0;
-        for(auto &i:piles){
-            hours+= ceil(double(i)/double(speed));
+    /*
+     1 2 2 3   
+    [3,6,7,11]
+    */
+    bool canEat(int bananasPerHour,vector<int>& piles, int limit){
+        double hours=0;
+        for(int bananas:piles){
+            hours+= ceil(double (bananas)/ double (bananasPerHour));
             if(hours>limit) return false;
         }
         return true;
     }
-
-    int minEatingSpeed(vector<int>& piles, int k) {
+    
+    
+    int minEatingSpeed(vector<int>& piles, int H) {
         
-        int n=piles.size();
-        ll l=1;
-        ll h=*max_element(piles.begin(),piles.end());
-        ll ans=1e9;
+        int l=1;
+        int h=*max_element(piles.begin(),piles.end());
+        int ans=0;
+        
         while(l<=h){
-
-            ll m=l+(h-l)/2;
-
-            if(func(m,piles,k)){
+            
+            int m=l+((h-l)/2);
+            
+            if(canEat(m,piles,H)){
+                ans=m;
                 h=m-1;
-                ans=min(ans,m);
             }else{
                 l=m+1;
             }
         }
         return ans;
-
     }
 };
