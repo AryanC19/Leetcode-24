5d9d3bbdcc5c8dd28fe51bdac62da06bbe38d5b5
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-01-28 11:04:10 +0530

    Time: 20 ms (29.6%), Space: 99.3 MB (32.4%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    int maxFish=0;
-    int m,n;
-    void func(int i,int j,vector<vector<int>>& grid,vector<vector<int>> &vis){
+    int maxFish = 0;
+    int m, n;
+    
+    int func(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& vis) {
+        if (i < 0 || j < 0 || i >= m || j >= n || vis[i][j] || grid[i][j] == 0)
+            return 0;
         
-        if(i>=m || j>=n) return;
-        vis[i][j]=1;
-        const vector<pair<int,int> > dirs= {{0,1},{1,0},{-1,0},{0,-1}};
-
-        int ans=grid[i][j];
-        for(auto &dir:dirs){
-            int ni=dir.first+i;
-            int nj=dir.second+j;
-            if(ni>=0 && nj>=0 && ni<m && nj<n && !vis[ni][nj] && grid[ni][nj]>0 ){
-                ans+=grid[ni][nj];
-                func(ni,nj,grid,vis);
-            }
+        vis[i][j] = 1;
+        int ans = grid[i][j];
+        
+        const vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        for (auto& dir : dirs) {
+            int ni = dir.first + i;
+            int nj = dir.second + j;
+            ans += func(ni, nj, grid, vis);
         }
-        maxFish=max(maxFish,ans);
+        
+       // vis[i][j] = 0; 
+        return ans;
     }
+    
     int findMaxFish(vector<vector<int>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
-        vector<vector<int>> vis(m,vector<int> (n,0));
-
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]>0){
-                    func(i,j,grid,vis);
+        m = grid.size();
+        n = grid[0].size();
+        vector<vector<int>> vis(m, vector<int>(n, 0));
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] > 0) {
+                    maxFish = max(maxFish, func(i, j, grid, vis));
                 }
             }
         }
+        
         return maxFish;
-
-
     }
 };
