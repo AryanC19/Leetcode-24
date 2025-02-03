068cd92fd5d7a6e08e4ef5fb42b53fda8f81921a
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-02-03 18:11:08 +0530

    Time: 0 ms (100%), Space: 95 MB (93.77%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-//linear tc and cosntast sc
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
-        int tot = n + m;
-        int ind = tot / 2;
+        int n = nums1.size(), m = nums2.size();
+        int k = n + m, midInd = k / 2;
+        int mid = 0, preMid = 0, i = 0, j = 0, cnt = 0;
 
-        double ans = 0;
-        bool even = (tot % 2 == 0);
-        
-        int prev1 = 0, prev2 = 0;
-        int i = 0, j = 0, cnt = 0;
-        
         while (i < n || j < m) {
-            int curr;
-            
-            if (i < n && (j >= m || nums1[i] <= nums2[j])) {
-                curr = nums1[i++];
+            int val;
+            if (i < n && (j >= m || nums1[i] < nums2[j])) {
+                val = nums1[i++];
             } else {
-                curr = nums2[j++];
+                val = nums2[j++];
             }
-            
-            if (cnt == ind - 1) prev1 = curr;
-            if (cnt == ind) {
-                prev2 = curr;
+            if (cnt == midInd - 1) preMid = val;
+            if (cnt == midInd) {
+                mid = val;
                 break;
             }
             cnt++;
         }
 
-        return even ? (prev1 + prev2) / 2.0 : prev2;
+        return (k % 2 == 0) ? (mid + preMid) / 2.0 : mid;
     }
 };
