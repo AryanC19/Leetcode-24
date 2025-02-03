233c2b47dda865d6db34eec89e4bb3567de339b5
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-02-03 15:36:56 +0530

    Time: 0 ms (100%), Space: 28 MB (27.06%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -9,29 +9,17 @@ public:
         int n=nums.size();
 
         for(int i=1;i<n;i++){
-            if(nums[i-1]>=nums[i]) len=0;
+            if(nums[i-1]>=nums[i]) len=1;
             else len++;
             ans=max(ans,len);
         }
         len=1;
         for(int i=1;i<n;i++){
-            if(nums[i-1]<=nums[i]) len=0;
+            if(nums[i-1]<=nums[i]) len=1;
             else len++;
             ans=max(ans,len);
         }
 
-        len=1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]>=nums[i+1]) len=0;
-            else len++;
-            ans=max(ans,len);
-        }
-        len=1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<=nums[i+1]) len=0;
-            else len++;
-            ans=max(ans,len);
-        }
         return ans;
     }
 };
