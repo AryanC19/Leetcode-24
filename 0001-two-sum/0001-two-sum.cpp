e4f837d943fd4243959a8bd72caa36ac8c82df93
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-02-06 19:01:46 +0530

    Time: 2 ms (79.05%), Space: 14.9 MB (35.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,23 +3,18 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         int n=nums.size();
-        unordered_map<int,int> mpp;
+        //            sum,ind
+        unordered_map<int,int>  mpp;
 
-        for(int i=0;i<nums.size();i++){
-
+        for(int i=0;i<n;i++){
+           
             int rem=target-nums[i];
-
-
-            if(mpp.find(rem)!=mpp.end()){
+            if(mpp.count(rem)){
                 return {i,mpp[rem]};
             }
-
-
             mpp[nums[i]]=i;
         }
 
-
-        return {-1,-1};
-        
+        return {};
     }
 };
