4872890e0b232f7c0e8ffe8eb0de3ebeda5420eb
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-02-11 23:40:25 +0530

    Time: 35 ms (15.03%), Space: 21 MB (9.69%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -2,62 +2,60 @@ class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         
-        //floydWarshal
+        // TC: O(V (E + V) log V + V²) 
+        // SC :  O(V² + E).
+
+        vector<vector<int> > mat;
 
 
-        int cnt=0;
+        unordered_map<int,vector<pair<int,int>> > adjList;
 
-        int city=-1;
-        int cntMax=0;
-
-
-        vector<vector<int> > dist (n,vector<int> (n,INT_MAX));
-
-
-        for(auto i:edges){
+        for(auto &i:edges){
             int u=i[0];
             int v=i[1];
             int wt=i[2];
-            dist[u][v]=wt;
-            dist[v][u]=wt;
-        }
-        for(int i=0;i<n;i++){
-            dist[i][i]=0;
+            adjList[u].push_back({v,wt});
+            adjList[v].push_back({u,wt});
         }
 
-        // floyd warshal
-        for(int via=0;via<n;via++){
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(dist[i][via] == INT_MAX || dist[via][j] ==INT_MAX)
-                        continue;
-                    dist[i][j]= min(dist[i][j], dist[i][via]+dist[via][j]);
+
+        //dijsktra for each src. single source shortest path algo
+
+        for(int src=0;src<n;src++){
+            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>> > pq;
+            vector<int> dist(n,INT_MAX);
+            dist[src]=0;
+            //     dist,node
+            pq.push({0,src});
+            while(!pq.empty()){
+                auto [wt,node]=pq.top();
+                pq.pop();
+                for(auto &[child,childWt]:adjList[node]){    
+                    if(wt +childWt <dist[child]){
+                        dist[child]=wt+childWt;
+                        pq.push({wt +childWt , child});
+                    }
                 }
-            }
+            } 
+            mat.push_back(dist);
         }
-
-        /*now comupting on the distance matrix, we find the city 
-        which has the greatest numebr and the smallest num of neighbours withing     thershold dist
-        */
-
 
         int cntCity=n;
-        int cityNo=-1;
+        int city=-1;
 
-        for(int city=0;city<n;city++){
+        for(int i=0;i<n;i++){
 
             int cnt=0;
-            for(int adjCity=0;adjCity<n;adjCity++){
-                if(dist[city][adjCity]<=distanceThreshold) cnt++;
+            for(int j=0;j<n;j++){
+                if(mat[i][j] <= distanceThreshold) cnt++;
             }
-
+            // since i =0 -> n , the last updated city will be highest thus satisfying the condition 
+            // of highest city num when min cities r equal
             if(cnt<=cntCity){
                 cntCity=cnt;
-                cityNo=city;
+                city=i;
             }
         }
-
-        return cityNo;
-
+        return city;
     }
 };
