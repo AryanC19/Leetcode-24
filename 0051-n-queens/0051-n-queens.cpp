e8c0d63ce9d37a346181d64262bc51dca8dd6f18
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-02-27 02:28:49 +0530

    Time: 0 ms (100%), Space: 10.1 MB (76.46%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,60 +1,73 @@
 class Solution {
 public:
-    bool isSafe(int r, int c, vector<string>& board, int n) {
-        int tr = r, tc = c;
+
+    bool isValid(int r,int c,vector<string> &board,int n){
+
+        int tr=r;
+        int tc=c;
+
         // left
-        while (tc >= 0) {
-            if (board[tr][tc] == 'Q')
-                return false;
+        while(tc>=0){
+            if(board[tr][tc]=='Q') return false;
             tc--;
         }
 
-        // leftdiagup
-        tr = r, tc = c;
-        while (tr >= 0 && tc >= 0) {
-            if (board[tr][tc] == 'Q')
-                return false;
+        tr=r;
+        tc=c;
 
+        //leftbott
+
+        while(tc>=0 && tr>=0){
+            if(board[tr][tc]=='Q') return false;
+            tc--;
             tr--;
-            tc--;
-        }
-        tr = r, tc = c;
-        // leftdiagdwn
-        while (tr < n && tc >= 0) {
-            if (board[tr][tc] == 'Q')
-                return false;
-            tr++;
-            tc--;
         }
 
+        tr=r;
+        tc=c;
+
+        //leftup
+        while(tc>=0 && tr<n){
+            if(board[tr][tc]=='Q') return false;
+            tc--;
+            tr++;
+        }
         return true;
     }
-    void solve(int col, vector<string>& board, vector<vector<string>>& ans,
-               int n) {
-        // missed this too
-        if (col == n) {
+
+
+    void solve(int col,vector<string> &board, vector<vector<string> > &ans,int n){
+
+        if(col==n){
             ans.push_back(board);
-            return;
+            return ;
         }
-        // messed this loop, didnt remember to pass col in solve func
-        for (int row = 0; row < n; row++)
-            if (isSafe(row, col, board, n)) {
-                board[row][col] = 'Q';
-                solve(col + 1, board, ans, n);
-                board[row][col] = '.';
+
+        for(int row=0;row<n;row++){
+            if(isValid(row,col,board,n)){
+                board[row][col]='Q';
+                solve(col+1,board,ans,n);
+                board[row][col]='.';
             }
+        }
+
     }
 
+
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
+
+        vector<vector<string> > ans;
         vector<string> board;
-        string row = string(n, '.');
-        for (int i = 0; i < n; i++) {
-            // cout<<row<<endl;
-            board.push_back(row);
+        string s(n,'.');
+
+        
+        for(int i=0;i<n;i++){
+            board.push_back(s);
         }
 
-        solve(0, board, ans, n);
+        solve(0,board,ans,n);
+
         return ans;
+
     }
 };
