06467a52a8d5921ce32ac6295fbf647f85f676db
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-03-03 11:54:45 +0530

    Time: 3 ms (88.52%), Space: 127.7 MB (77.25%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
+
+    // O (1) space
+    // O(N)
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> low,high;
-        int cnt=0;
+        int n=nums.size();
+        int less=0,more=0,equal=0;
 
         for(auto i:nums){
-            if(i<pivot) low.push_back(i);
-            else if(i>pivot) high.push_back(i);
-            else cnt++;
+
+            if(i<pivot) less++;
+            else if(i>pivot) more++;
+            else equal++;
         }
-        while(cnt--) low.push_back(pivot);
-        for(auto i:high) low.push_back(i);
-        return low;
+        vector<int> ans(n,0);
+
+        int l=0,e=less,h=less+equal;
+        for(auto i:nums){
+            if(i<pivot) ans[l++]=i;
+            else if(i>pivot) ans[h++]=i;
+            else ans[e++]=i;
+        }
+        return ans;
     }
 };
