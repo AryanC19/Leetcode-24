0d4ff1585f1afe59c95cddf119eedeae6c24b58a
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-03-03 17:37:41 +0530

    Time: 37 ms (5.54%), Space: 27 MB (17.03%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+
+    int lcs(int i, int j, string &s1, string &s2) {
+        if (i < 0 || j < 0) return 0;
+        if (dp[i][j] != -1) return dp[i][j];
+        if (s1[i] == s2[j]) return dp[i][j] = 1 + lcs(i - 1, j - 1, s1, s2);
+        return dp[i][j] = max(lcs(i, j - 1, s1, s2), lcs(i - 1, j, s1, s2));
+    }
+
     string shortestCommonSupersequence(string s1, string s2) {
         int n = s1.size(), m = s2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-                if(s1[i-1] == s2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+        dp.assign(n, vector<int>(m, -1));  // Ensure dp size is (n x m)
+
+        int lcsLen = lcs(n - 1, m - 1, s1, s2);
+
+        int i = n - 1, j = m - 1;
+        string scs = "";
+
+        while (i >= 0 && j >= 0) {
+            if (s1[i] == s2[j]) {
+                scs += s1[i];
+                i--, j--;
+            } else {
+                if (j > 0 && (i == 0 || dp[i][j - 1] > dp[i - 1][j])) {
+                    scs += s2[j--];
                 } else {
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    scs += s1[i--];
                 }
             }
         }
-        
-        // Build SCS
-        string scs = "";
-        int i = n, j = m;
-        while(i > 0 && j > 0) {
-            if(s1[i-1] == s2[j-1]) {
-                scs += s1[i-1] ;
-                i--; j--;
-            } else if(dp[i-1][j] > dp[i][j-1]) {
-                scs += s1[i-1] ;
-                i--;
-            } else {
-                scs += s2[j-1] ;
-                j--;
-            }
-        }
-        
-        // Add remaining characters
-        while(i > 0) {
-            scs += s1[i-1] ;
-            i--;
-        }
-        while(j > 0) {
-            scs += s2[j-1];
-            j--;
-        }
-        reverse(scs.begin(),scs.end());
+
+        while (i >= 0) scs += s1[i--];
+        while (j >= 0) scs += s2[j--];
+
+        reverse(scs.begin(), scs.end());
         return scs;
     }
 };
