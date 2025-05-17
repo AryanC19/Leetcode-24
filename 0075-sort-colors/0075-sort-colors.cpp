8958e2042f2745bd40e5d93656743e3b85da8a7a
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-05-17 12:30:00 +0530

    Time: 5 ms (0.92%), Space: 10.1 MB (100%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
+    //selection sort O n^2
     void sortColors(vector<int>& arr) {
         
         for(int i=0;i<arr.size();i++){
-
-            for(int j=i;j<arr.size();j++){
-                if(arr[j]<arr[i]){
-                    swap(arr[j],arr[i]);
+            int mini=i;
+            for(int j=i+1;j<arr.size();j++){
+                if(arr[mini]>arr[j]){
+                    mini=j;
                 }
             }
+            swap(arr[mini],arr[i]);
 
         }
         
