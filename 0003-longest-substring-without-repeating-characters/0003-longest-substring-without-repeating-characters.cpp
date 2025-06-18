cde7a5d90e438b42279602e1e7f27499c966743c
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-06-18 22:47:11 +0530

    Time: 14 ms (42.1%), Space: 11.8 MB (58.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,6 +3,7 @@ public:
     int lengthOfLongestSubstring(string s) {
         
         int n=s.size();
+        
         unordered_map<char,int> mpp;
 
         int l=0,r=0;
@@ -11,8 +12,8 @@ public:
         while(r<n){
             if(mpp.find(s[r])!=mpp.end()){
                 l=max(l,mpp[s[r]]+1);
-                maxLen=max(maxLen,r-l+1);
             }
+            maxLen=max(maxLen,r-l+1);
             mpp[s[r]]=r;
             r++;
         }
