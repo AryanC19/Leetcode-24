9660d4ae1d1e9fc9216bf55e36e38e96a8bd827b
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-07-02 22:57:40 +0530

    Time: 4 ms (70.36%), Space: 11.5 MB (84.51%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    //RUPEEK COUDNT SOLVE FUCKIGN IDIOt 
-    vector<int> findAnagrams(string text, string word) {
-        unordered_map<char, int> wordMap, textMap;
+    bool isZero(unordered_map<char, int> &freqMap){
+        for(auto &it:freqMap){
+            if(it.second!=0) return false;
+        }
+        return true;
+    }
+    vector<int> findAnagrams(string s, string p) {
+        unordered_map<char, int> freqMap;
 
-        for(auto c: word) wordMap[c]++;
-        int wordSize=word.size();
-        int textSize=text.size();
-
+        for(auto i:p) freqMap[i]++;
+        int l=0,r=0;
+        int n=s.size();
+        int k=p.size();
         vector<int> ans;
-        int l=0, r=wordSize;
+        while(r<n){
 
-        for(int i=0;i<wordSize;i++){
-            textMap[text[i]]++;
-        }
-        if(textMap==wordMap) ans.push_back(0);
+            freqMap[s[r]]--;
 
-        while(r<textSize){
-            textMap[text[r]]++;
-            textMap[text[l]]--;
-            if(textMap[text[l]]==0) textMap.erase(text[l]);
+            if(r-l+1==k){
+                if(isZero(freqMap)) ans.push_back(l);
+                freqMap[s[l]]++;
+                l++;
+            }
             r++;
-            l++;
-            if(textMap==wordMap) ans.push_back(l);
         }
-
         return ans;
     }
 };
