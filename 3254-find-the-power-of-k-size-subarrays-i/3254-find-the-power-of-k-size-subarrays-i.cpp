836c0fd4f200d2203f88bd7b13fde3d64360b303
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-07-12 16:57:51 +0530

    Time: 4 ms (27.84%), Space: 34.2 MB (70.55%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> ans(n - k + 1, -1);
+        int n=nums.size();
 
-        // Handle k == 1 directly
-        if (k == 1) {
-            for (int i = 0; i < n; i++) ans[i] = nums[i];
-            return ans;
+        int consCnt=1;// count of consecutive elemnts
+
+        
+
+        vector<int> ans(n-k+1,-1);
+       
+        for (int i = 1; i < k; i++) {
+            if(nums[i]==nums[i-1]+1) consCnt++;
+            else consCnt=1;
         }
 
-        int l = 0, r = 1;
-        int consecPairs = 0;  // counts nums[i]–nums[i‑1]==1 in current window
 
-        while (r < n) {
-            // Add the new pair (r-1, r)
-            if (nums[r] - nums[r - 1] == 1) consecPairs++;
+        if(consCnt==k) ans[0]=nums[k-1];
 
-            // When window [l..r] is size k
-            if (r - l == k - 1) {
-                // All k-1 pairs must be consecutive
-                if (consecPairs == k - 1) {
-                    ans[l] = nums[r];
-                }
-                // Remove the leftmost pair (l, l+1) before sliding
-                if (nums[l + 1] - nums[l] == 1) consecPairs--;
-                l++;
+        int l=1, r=k;
+
+
+        while(r<n){
+
+            
+            if(nums[r]-nums[r-1] ==1) consCnt++; 
+            else consCnt=1;
+
+        
+            if(consCnt>=k){
+                ans[l]=nums[r];
             }
+
+            l++;            
             r++;
         }
-
         return ans;
     }
 };
