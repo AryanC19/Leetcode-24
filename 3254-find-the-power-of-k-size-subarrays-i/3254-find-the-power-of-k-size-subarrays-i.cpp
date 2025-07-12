5c2037343e01067878582b0693b72833a8943774
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-07-12 18:11:38 +0530

    Time: 1 ms (50.73%), Space: 35.2 MB (16.33%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
+         
         int n=nums.size();
-
-        int consCnt=1;// count of consecutive elemnts
-
-        
-
+        deque<int> dq;
+        int r=0;
         vector<int> ans(n-k+1,-1);
-       
-        for (int i = 1; i < k; i++) {
-            if(nums[i]==nums[i-1]+1) consCnt++;
-            else consCnt=1;
-        }
-
-
-        if(consCnt==k) ans[0]=nums[k-1];
-
-        int l=1, r=k;
-
-
         while(r<n){
-
-            
-            if(nums[r]-nums[r-1] ==1) consCnt++; 
-            else consCnt=1;
-
-        
-            if(consCnt>=k){
-                ans[l]=nums[r];
+            if(!dq.empty() && nums[r]!= dq.back ()+1) dq.clear();
+            dq.push_back(nums[r]);
+            if(dq.size()>=k){
+                ans[r-k+1]=dq.back();
+                dq.pop_front();
             }
-
-            l++;            
             r++;
         }
         return ans;
