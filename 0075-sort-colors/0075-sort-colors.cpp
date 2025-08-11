86024255454ce9218d8f3c74012ba2037f3a438c
Aryan C <89149374+AryanC19@users.noreply.github.com>
2025-08-11 23:18:04 +0530

    Time: 3 ms (2.99%), Space: 15.5 MB (11.98%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,18 +1,58 @@
 class Solution {
 public:
-    //selection sort O n^2
-    void sortColors(vector<int>& arr) {
-        
-        for(int i=0;i<arr.size();i++){
-            int mini=i;
-            for(int j=i+1;j<arr.size();j++){
-                if(arr[mini]>arr[j]){
-                    mini=j;
-                }
-            }
-            swap(arr[mini],arr[i]);
 
-        }
+    void mergeFunc(vector<int> &nums, int l, int m , int r){
         
+        int s1= m-l+1;
+        int s2= r-(m+1)+1;
+        
+
+        vector<int> arr1(s1), arr2(s2);
+
+        for(int i=0;i<s1;i++){
+            arr1[i]=nums[l+i];
+        }
+
+
+        for(int i=0;i<s2;i++){
+            arr2[i]=nums[m+1+i];
+        }
+
+        int k=l;
+        int i=0,j=0;
+        while(i<s1 && j<s2){
+            if(arr1[i] < arr2[j]){
+                nums[k++]=arr1[i++];
+            }else{
+                nums[k++]=arr2[j++];
+            }
+        }
+
+        while(i<s1){
+           
+            nums[k++]=arr1[i++];
+            
+        }
+
+
+        while( j<s2){
+            nums[k++]=arr2[j++];
+            
+        }
+
+    }
+    void mergeSort(vector<int> &nums, int l, int r){
+
+        if(l>=r) return;
+        int m= l+(r-l)/2;
+
+        mergeSort(nums,l,m );
+        mergeSort(nums,m+1, r);
+        mergeFunc(nums, l, m , r);
+
+    }
+    void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        mergeSort(nums, 0, n-1);
     }
 };
