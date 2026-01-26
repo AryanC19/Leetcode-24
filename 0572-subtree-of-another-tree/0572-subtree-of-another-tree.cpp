5f20b2f6897ac0022bbf77b5c05236c6c44e1bdc
Aryan C <89149374+AryanC19@users.noreply.github.com>
2026-01-27 01:46:58 +0530

    Time: 23 ms (5.19%), Space: 42.2 MB (5.01%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -11,29 +11,35 @@
  */
 class Solution {
 public:
-
-    string buildString(TreeNode* root){
+    string path(TreeNode * root){
 
         if(root==NULL) return "#";
 
-        string left=buildString(root->left);
-        string right=buildString(root->right);
-
-        return to_string(root->val)+","+left+","+right;
+        string left= path(root->left);
+        string right=path(root->right);
+        // "^" vv imp, prefix boundary to save from
+        // 12#1#1 , and 2#1#1 . now ^12#1#1, and ^2#1#1 , mismatch at 2!=1
+        return "^"+to_string(root->val)+","+left+","+right;
     }
-    void func(TreeNode* root, set <string> &st){
-        if(root==NULL) return;
-        st.insert(buildString(root));
-        func(root->left,st);
-        func(root->right,st);
-    }
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
-        
-        string needed=buildString(subRoot);
-        set<string> st;
+    bool isSubtree(TreeNode* rt, TreeNode* subRt) {
+        string root=path(rt);
+        string subRoot=path(subRt);
+        int i=0;
+        while(i<root.size()){
+            int j=0;
+            if(subRoot[j]==root[i]){
+                int t_i=i;
+                while(j< subRoot.size() && t_i < root.size() && subRoot[j]==root[t_i]){
+                    t_i++;
+                    j++;
+                }
+            }else{
+                j=0;
+            }
+            if(j==subRoot.size()) return true;
+            i++;
+        }
+        return false;
 
-        func(root, st);
-
-        return st.count(needed);
     }
 };
