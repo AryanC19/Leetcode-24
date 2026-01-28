e34698f3c70b485c4bd34aff135d9f4e3d6a6833
Aryan C <89149374+AryanC19@users.noreply.github.com>
2026-01-28 09:08:23 +0530

    Time: 4 ms (87.37%), Space: 22.8 MB (76.57%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
-    /*
-     1 2 2 3   
-    [3,6,7,11]
-    */
-    bool canEat(int bananasPerHour,vector<int>& piles, int limit){
-        double hours=0;
-        for(int bananas:piles){
-            hours+= ceil(double (bananas)/ double (bananasPerHour));
-            if(hours>limit) return false;
+/*
+[3,6,7,11] k=4
+ 1 2 2 3 =8h
+*/
+
+    bool canEat(int bananasPerHour,vector<int>& piles, int maxAllowedHours){
+        int hours=0;
+        for(int &bananas: piles){
+            // ceil , 4%3 -> rem of 4/3
+            if(bananas % bananasPerHour !=0){
+                hours+= (bananas/bananasPerHour)+1;
+            }else{
+                hours+=(bananas/bananasPerHour);
+            }
+            if(hours>maxAllowedHours) return false;
         }
-        return true;
+        return hours<=maxAllowedHours;
     }
-    
-    
-    int minEatingSpeed(vector<int>& piles, int H) {
+    int minEatingSpeed(vector<int>& piles, int h) {
         
-        int l=1;
-        int h=*max_element(piles.begin(),piles.end());
-        int ans=0;
-        
-        while(l<=h){
-            
-            int m=l+((h-l)/2);
-            
-            if(canEat(m,piles,H)){
-                ans=m;
-                h=m-1;
+        int minBananas=1;
+        int maxBananas=*max_element(piles.begin(),piles.end());
+
+        // for(int bananas=minBananas;bananas<=maxBananas;bananas++){
+        //     if(canEat(bananas, piles, h)) return bananas;
+        // }
+
+        int l=minBananas;
+        int r=maxBananas;
+
+        int minSpeed=0;
+        while(l<=r){
+            int m= l+(r-l)/2;
+
+            if(canEat(m,piles,h)){
+                r=m-1;
+                minSpeed=m;
             }else{
                 l=m+1;
             }
         }
-        return ans;
+
+        return minSpeed;
     }
 };
