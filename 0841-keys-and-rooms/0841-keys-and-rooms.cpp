a395729156c1d6eb8539af64b30cb8c626b885c5
Aryan C <89149374+AryanC19@users.noreply.github.com>
2026-01-28 15:33:48 +0530

    Time: 0 ms (100%), Space: 14.4 MB (71.67%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
 
-    void dfs(int node,vector<int> &vis,unordered_map<int,vector<int> > &mpp){
-
-        vis[node]=1;
-        for(auto &child:mpp[node]){
-            if(!vis[child]) dfs(child,vis,mpp);
+/**/
+    void func(int i, vector<vector<int>>& rooms , vector<int >& vis){
+        if(vis[i]) return;
+        vis[i]=1;
+        for(int keys: rooms[i]){
+            func(keys, rooms,vis);
         }
     }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        unordered_map<int,vector<int> > mpp;
-        
+        int n=rooms.size();
+        vector<int> vis(n);
+        func(0,rooms,vis);
 
-        for(int i=0;i<rooms.size();i++){
-            
-            for(auto &j:rooms[i]){
-                mpp[i].push_back(j);
-            }
+        for(int i=0;i<n;i++){
+            if(vis[i]==0) return false;
         }
-
-
-        vector<int> vis(rooms.size(),0);
-
-        dfs(0,vis,mpp);
-
-        for(auto i:vis) if(i==0) return false;
         return true;
-        
     }
 };
